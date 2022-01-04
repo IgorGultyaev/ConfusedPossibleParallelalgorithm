66a8b402597e6cf648aa7f34aad9127e44fc9076
asp-gin <igor@gultyaev.ru>
2022-01-04 22:50:19 +0300

    Пример

---
+++ src/com/company/Figure.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Figure {
+
+    protected String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public double getPerimeter() {
+        return 0.0;
+    }
+
+    public double getArea() {
+        return 0.0;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
